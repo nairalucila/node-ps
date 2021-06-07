17b0c04202384585d98889199bd529c3030d8699
nairalucila <nairacoppari@gmail.com>
2021-06-07 12:12:15 -0300

    se crearon scripts para crear admin y roles por defecto

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
 const port = 3000;
-const db = require('./database/database');
+const db = require('./database/index');
 const { checkToken } = require('./middleware');
-const {createAdmin, createRols} = require('./database/asotiation')
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
